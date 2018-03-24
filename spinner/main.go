6d414d29a9c5fad9ccf487d99a83a193ff5f70cc
champkeh <champion.ge@gmail.com>
2018-03-24 11:58:40 +0800

    update spinner

--- spinner/main.go
+++ spinner/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+// 使用一个单独的 go routine 来执行这个函数
+// delay参数用于控制转动的快慢
 func spinner(delay time.Duration) {
 	for {
 		for _, r := range `-\|/` {
@@ -16,7 +18,10 @@ func spinner(delay time.Duration) {
 
 func main() {
 	go spinner(200 * time.Millisecond)
+
+	// 暂停10秒，模拟程序的执行
 	time.Sleep(10 * time.Second)
+
 	fmt.Print("\r")
 	fmt.Println("program end.")
 }
