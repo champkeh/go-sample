6c02d06b07e5bc2cdae1d29a2888ada210526071
champkeh <champion.ge@gmail.com>
2018-08-02 10:27:01 +0800

    use struct to encapsulates the icmp datagram instead of []byte directly

--- icmp/main.go
+++ icmp/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
+	"bytes"
 )
+
+// ICMP represent icmp protocol datagram
+type ICMP struct {
+	Type        uint8
+	Code        uint8
+	Checksum    uint16
+	Identifier  uint16
+	SequenceNum uint16
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -22,29 +33,33 @@ func main() {
 	conn, err := net.DialIP("ip4:icmp", nil, addr)
 	checkError(err)
 
-	var msg [512]byte
-	msg[0] = 8    // type
-	msg[1] = 0    // code
-	msg[2] = 0    // checksum, fix later
-	msg[3] = 0    // checksume, fix later
-	msg[4] = 0    // identifier[0]
-	msg[5] = 14   // identifier[1]
-	msg[6] = 0    // sequence[0]
-	msg[7] = 237  // sequence[1]
-	len := 8
+	icmp := ICMP{
+		Type:        8,
+		Code:        0,
+		Checksum:    0,
+		Identifier:  14,
+		SequenceNum: 237,
+	}
 
-	sum := checkSum(msg[0:len])
-	msg[2] = byte(sum >> 8)
-	msg[3] = byte(sum & 255)
+	var buffer bytes.Buffer
 
-	_, err = conn.Write(msg[0:len])
+	// 将icmp数据报按照大端序写入buffer,进行校验和的计算
+	binary.Write(&buffer, binary.BigEndian, icmp)
+	icmp.Checksum = checkSum(buffer.Bytes())
+
+	// 然后清空buffer，将计算完校验和的icmp数据报重新写入buffer
+	buffer.Reset()
+	binary.Write(&buffer, binary.BigEndian, icmp)
+
+	_, err = conn.Write(buffer.Bytes())
 	checkError(err)
 
-	n, err := conn.Read(msg[0:])
+	msg := make([]byte, 512)
+	n, err := conn.Read(msg)
 	checkError(err)
 
 	fmt.Printf("Got response %d bytes\n", n)
-	// 在windows上测试，会返回28个字节
+	// 这里会返回28个字节，不知道是什么原因
 	// 这里进行截断，取最后8个字节
 	if n >= 8 {
 		n -= 8
